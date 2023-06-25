58d3f93574e024190b76838d81f64466356bdbf0
chandraKamalSingh7 <ckdnwr4257@gmail.com>
2023-06-25 23:21:14 +0545

    DSA

--- 14.Data structure and Algorithms/Array as ADTS/Q2_sumOfSubArr.cpp
+++ 14.Data structure and Algorithms/Array as ADTS/Q2_sumOfSubArr.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int mod=1e9+7;
 
 int main(){
     int n;
@@ -9,16 +10,23 @@ int main(){
     {
         cin>>arr[i];
     }
-    int output=0;
-    for(int i=0;i<n;i++){
-        output=0;
-        for(int j=i;j<n;j++){
-            output+=arr[j];
-            cout<<output<<endl;
+    // int output=0;
+    // int sum=0;
+    // for(int i=0;i<n;i++){
+    //     output=0;
+    //     for(int j=i;j<n;j++){
+    //         output+=arr[j];
+    //         sum+=output;
+    //         cout<<output<<endl;
+    //     }   
+    // }
+    // cout<<sum<<endl;
+   int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=(sum+(n-i)*(i+1)*arr[i])%mod;
         }
-        
-    }
-    
+        cout<<sum%mod<<endl;
     
 
     return 0;
